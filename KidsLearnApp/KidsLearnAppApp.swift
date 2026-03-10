7947257ec64da067df5df6dd98dc5089c774be63
udayammi11 <88530120+udayammi11@users.noreply.github.com>
2026-03-10 22:51:25 +0530

    Initial Commit

---
+++ KidsLearnApp/KidsLearnAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  KidsLearnAppApp.swift
+//  KidsLearnApp
+//
+//  Created by Uday Kumar on 10/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct KidsLearnAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
